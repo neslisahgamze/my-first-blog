a52b3a30cc3c453c06923044a5bc1f57bb892105
neslisahgamze <neslisahgamze@gmail.com>
2017-03-26 23:36:45 +0300

    Detaylı blog gönderileri için CSS'e ilaveten view ve template eklendi.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ghd(7drq=ke*wu^-6#o14q2pi-kin%!qso9-6m@a(ct)6xw0si'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
